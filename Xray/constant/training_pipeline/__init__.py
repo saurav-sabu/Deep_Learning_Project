90f1767e8751d30fee32eb55167e4f2e8834cd66
Saurav Sabu <saurav.sabu9@gmail.com>
2024-02-07 14:28:31 +0530

    train config added

---
+++ Xray/constant/training_pipeline/__init__.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import List
+
+import torch
+
+TIMESTAMP: datetime = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+ARTIFACT_DIR: str = "artifacts"
+BUCKET_NAME: str = "XrayLungImage"
+S3_DATA_FOLDER: str = "data"
+
+CLASS_LABEL_1 : str = "NORMAL"
+CLASS_LABEL_2 : str = "PNEUMONIA" 
---
+++ Xray/pipeline/training_pipeline.py
@@ -0,0 +1,42 @@
+import sys
+import os
+
+from Xray.components.data_ingestion import DataIngestion
+from Xray.entity.artifact_entity import DataIngestionArtifact
+from Xray.entity.config_entity import DataIngestionConfig
+from Xray.exception import XRayException
+
+class TrainPipeline:
+
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+    
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        logging.info("Start data ingestion method")
+
+        try:
+            logging.info("Getting the data from S3")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config = self.data_ingestion_config
+            )
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+            logging.info("Got the train set and test set from S3")
+
+            logging.info("Exited start_data_ingestion method")
+
+            return data_ingestion_artifact
+        
+        except Exception as e:
+            raise XRayException(e,sys)
+        
+
+if __name__ == "__main__":
+    train_pipeline = TrainPipeline()
+    train_pipeline.start_data_ingestion()
+
+
+
+
